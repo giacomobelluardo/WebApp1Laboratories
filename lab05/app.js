e834580df0f4d56f0a711135dce41dc862beaa7c
giacomobelluardo <giacomino21@icloud.com>
2024-04-18 10:47:51 +0200

    Point 1 almost finished

--- lab05/app.js
+++ lab05/app.js
@@ -1,3 +1,111 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', event=>{
+    console.log('Page loaded')
+
+    const listFilms = document.getElementById("films-list")
+
+    for(const film of l1.films){
+        const li = document.createElement("li")
+        li.classList.add("list-group-item")
+
+        listFilms.appendChild(li)
+
+        //Row
+        const div = document.createElement("div")
+        div.classList.add("row")
+        li.appendChild(div)
+
+        //Title
+        const title = document.createElement("div")
+        title.classList.add("col-6", "col-xl-3", "favorite-title", "d-flex", "gap-2", "align-items-center")
+        title.innerText = film.title
+        div.appendChild(title)
+
+        //Is Favorite
+        const fav = document.createElement("div")
+        fav.classList.add("col-3", "text-center")
+        const span = document.createElement("span")
+        fav.appendChild(span)
+        span.classList.add("custom-control", "custom-checkbox")
+        div.appendChild(fav)
+        
+        if(film.favorite == true){
+            span.innerHTML = `
+                <input type="checkbox" class="custom-control-input" checked>
+                <label class="custom-control-label" for="films[1][favorite]">Favorite</label>
+            `
+        }
+        else{
+            span.innerHTML = `
+            <input type="checkbox" class="custom-control-input" >
+            <label class="custom-control-label" for="films[1][favorite]">Favorite</label>
+        `
+        }
+
+        //Date
+        const date = document.createElement("div")
+        date.classList.add("col-3", "text-center")
+        date.innerText = film.date.format('YYYY-DD-MM')
+        div.appendChild(date)
+
+        //Ratings
+        const rating = document.createElement("div")
+        rating.classList.add("actions-container", "col-3", "text-center")
+        div.appendChild(rating)
+        const stars = document.createElement("div")
+        stars.classList.add("rating")
+        for(let i=0; i<film.score; i++){
+            stars.innerHTML = `<i class="bi bi-star-fill"></i>`
+        }
+        for(let i=0; i<(5-film.score); i++){
+            stars.innerHTML = `<i class="bi bi-star"></i>`
+        }
+        rating.appendChild(stars)
+
+        const actions = document.createElement("div")
+        actions.classList.add("actions")
+        rating.appendChild(actions)
+
+        const buttonPencil = document.createElement("i")
+        const buttonTrash = document.createElement("i")
+        buttonPencil.classList.add("bi", "bi-pencil")
+        buttonTrash.classList.add("bi", "bi-trash")
+        actions.appendChild(buttonPencil)
+        actions.appendChild(buttonTrash)
+    }
+})
+
+function Film(id, title, favorite=false, date=null, score=null, user=1) {
+    this.id = id;
+    this.title = title;
+    this.favorite = favorite;
+    this.date = date;
+    this.score = score;
+    this.user = user;
+
+    this.printFilm = () =>{
+        let string = "";
+        string += "Id: "+ this.id;
+        string += ", Title: " + this.title;
+        string += ", Favorite: " + this.favorite;
+        string += ", Watch date: " 
+        if(this.date != null)
+            string += this.date.format('MMMM D, YYYY');
+        else
+            string += this.date;
+        string += ", Score: " + this.score;
+        string += ", User: " + this.user;
+
+        return string;
+    }
+
+    this.deleteDate = () =>{
+        if(this.date != null)
+            this.date = null;
+    }
+}
+
 function FilmLibrary(){
     this.films = [];
 
@@ -49,3 +157,14 @@ function FilmLibrary(){
         })
     }
 }
+
+const f1 = new Film(1, 'Pulp fiction', true, dayjs('2024-03-14'), 7);
+const f2 = new Film(2, 'Taxi Driver', undefined, dayjs('2024-03-11'), 9, 5);
+const f3 = new Film(3, 'Star Wars', true, dayjs('2024-02-12'), 10);
+
+const l1 = new FilmLibrary();
+
+l1.addFilm(f1);
+l1.addFilm(f2);
+l1.addFilm(f3);
+
