360bfe87a1779f282d01763b2b44408fcb8dad32
giacomobelluardo <giacomino21@icloud.com>
2024-05-23 11:29:18 +0200

    Adding filters

--- lab09/simple-client/app.js
+++ lab09/simple-client/app.js
@@ -9,12 +9,7 @@ document.addEventListener('DOMContentLoaded', event => {
             const response = await fetch('http://localhost:3001/api/films')
             if (response.ok) {
                 const films = await response.json()
-                const div = document.getElementById('filmList')
-                div.innerHTML = `<p>We have ${films.length} films</p>`
-                for (const f of films) {
-                    console.log(f)
-                    div.innerHTML += `<p>${f.title}</p>`
-                }
+                writeOnPage(films, "All")
             } else {
                 console.log('http error code', response.status)
             }
@@ -23,4 +18,73 @@ document.addEventListener('DOMContentLoaded', event => {
         }
     })
 
+    //FILTER BUTTONS
+    document.getElementById('filterbtn1').addEventListener('click', async () => {
+        // on button click
+        try {
+            const response = await fetch('http://localhost:3001/api/films/favorite')
+            if (response.ok) {
+                const films = await response.json()
+                writeOnPage(films, "Favorite")
+            } else {
+                console.log('http error code', response.status)
+            }
+        } catch (e) {
+            console.log('Network error')
+        }
+    })
+
+    document.getElementById('filterbtn2').addEventListener('click', async () => {
+        // on button click
+        try {
+            const response = await fetch('http://localhost:3001/api/films/ratings')
+            if (response.ok) {
+                const films = await response.json()
+                writeOnPage(films, "Best rated")
+            } else {
+                console.log('http error code', response.status)
+            }
+        } catch (e) {
+            console.log('Network error')
+        }
+    })
+
+    document.getElementById('filterbtn3').addEventListener('click', async () => {
+        // on button click
+        try {
+            const response = await fetch('http://localhost:3001/api/films/lastMonth')
+            if (response.ok) {
+                const films = await response.json()
+                writeOnPage(films, "Last month")
+            } else {
+                console.log('http error code', response.status)
+            }
+        } catch (e) {
+            console.log('Network error')
+        }
+    })
+
+    document.getElementById('filterbtn4').addEventListener('click', async () => {
+        // on button click
+        try {
+            const response = await fetch('http://localhost:3001/api/films/unseen')
+            if (response.ok) {
+                const films = await response.json()
+                writeOnPage(films, "Unseen")
+            } else {
+                console.log('http error code', response.status)
+            }
+        } catch (e) {
+            console.log('Network error')
+        }
+    })
 });  
+
+function writeOnPage(films, textIntro){
+    const div = document.getElementById('filmList')
+    div.innerHTML = `<p>${text} films</p>`
+    for (const f of films) {
+        console.log(f)
+        div.innerHTML += `<p>${f.title}</p>`
+    }
+}
